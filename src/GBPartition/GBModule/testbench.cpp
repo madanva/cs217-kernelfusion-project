b26a276ad10bea3f239135a987ee9408d79ef796
Pullabhatla Smriti <psmriti@caddy08.stanford.edu>
2026-01-31 00:08:56 -0800

    almost there

--- src/GBPartition/GBModule/testbench.cpp
+++ src/GBPartition/GBModule/testbench.cpp
@@ -52,38 +52,166 @@
    #pragma CTC SKIP
 #endif
 
+std::vector<double> golden_y;
+
 SC_MODULE(Source) {
   sc_in<bool> clk;
   sc_in<bool> rst;  
   Connections::Out<spec::StreamType> data_in;  
-  Connections::Out<bool> pe_done;
+  Connections::Out<bool> pe_done; 
   Connections::Out<spec::Axi::SubordinateToRVA::Write> rva_in;
 
-  std::vector<spec::Axi::SubordinateToRVA::Write> src_vec;
- 
   SC_CTOR(Source) {
-    SC_THREAD(run);
+    SC_THREAD(layernorm_run);
     sensitive << clk.pos();
     async_reset_signal_is(rst, false);
   }
 
-  void run() {
+  void layernorm_run() {
     data_in.Reset();
     pe_done.Reset();
     rva_in.Reset();
-
     spec::Axi::SubordinateToRVA::Write  rva_in_src;
 
-    wait();
-    for (unsigned i = 0; i < src_vec.size(); i++) {
-      if (src_vec[i].rw == 1) {
-        cout << hex << sc_time_stamp() << " Source rva write data " << src_vec[i].data << endl;
-      }
-    
-      rva_in.Push(src_vec[i]);
-      wait(1);
+    spec::VectorType act_vec;
+    spec::VectorType gamma_vec;
+    spec::VectorType beta_vec;
+
+    AdpfloatType<8, 3> adpfloat_tmp;
+    spec::AdpfloatBiasType adpbias_input = 2;
+    spec::AdpfloatBiasType adpbias_beta = 0;
+    spec::AdpfloatBiasType adpbias_gamma = 1;
+
+        // --- Data Generation ---
+    const int num_vectors = 16; // Number of vectors in the 16x16 matrix (rows)
+    const int vector_size = 16; // Size of each vector (columns)
+    std::vector<std::vector<double>> input_matrix(num_vectors, std::vector<double>(vector_size));
+    std::vector<double> gamma_vector(vector_size);
+    std::vector<double> beta_vector(vector_size);
+
+    for (int i = 0; i < num_vectors; ++i) {
+        for (int j = 0; j < vector_size; ++j) {
+            input_matrix[i][j] = ((double)rand() / RAND_MAX * 2.0 - 1.0);
+        }
     }
-  } // run()
+    for (int i = 0; i < vector_size; ++i) {
+        gamma_vector[i] = ((double)rand() / RAND_MAX * 2.0 - 1.0);
+        beta_vector[i] = ((double)rand() / RAND_MAX * 2.0 - 1.0);
+    }
+
+    // get golden_y layer norm for input_matrix, gamma_vector and beta_vector
+    const double epsilon = 1e-5;
+    for (int i = 0; i < num_vectors; ++i) { // For each input vector (row)
+        // 1. Calculate mean
+        double sum = 0.0;
+        for (int j = 0; j < vector_size; ++j) {
+            sum += input_matrix[i][j];
+        }
+        double mean = sum / vector_size;
+
+        // 2. Calculate variance
+        double sq_sum = 0.0;
+        for (int j = 0; j < vector_size; ++j) {
+            sq_sum += (input_matrix[i][j] - mean) * (input_matrix[i][j] - mean);
+        }
+        double variance = sq_sum / vector_size;
+        double std_dev = sqrt(variance + epsilon);
+
+        // 3. Normalize, scale, and shift, then store in golden_y
+        for (int j = 0; j < vector_size; ++j) {
+            double normalized = (input_matrix[i][j] - mean) / std_dev;
+            double golden_val = normalized * gamma_vector[j] + beta_vector[j];
+            golden_y.push_back(golden_val);
+        }
+    }
+
+
+    wait();
+
+    //LayerNorm AXI GBControlConfig
+    rva_in_src.rw = 1;
+    rva_in_src.data = set_bytes<16>("01_00_00_02_00_00_00_10_00_01_00_00_00_00_00_01"); //is_valid=1, mode=0, is_rnn=0, memory_index=0, output_memory_index=0, num_vector= 16, num_output_vector=0, num_timestep=256, num_timestep_padding=0, adpbias_act=2, adpbias_atten=0, adpbias_beta = 0, adpbias_gamma=1
+    rva_in_src.addr = set_bytes<3>("90_00_10");  // last 4 bits never used 
+    rva_in.Push(rva_in_src);
+    wait(); 
+
+    //GBCore LargeBuffer Config
+    rva_in_src.rw = 1;
+    rva_in_src.data = set_bytes<16>("00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_10"); //num_vector_large for kMaxNumManagers=0 is 16, base_large for kMaxNumManagers=0 is 0 
+    rva_in_src.addr = set_bytes<3>("40_00_10");  // last 4 bits never used 
+    rva_in.Push(rva_in_src);
+    wait(); 
+
+    // Load Input data to LargeBuffer (starting at address 0x500000 + 0)
+    for (int i = 0; i < num_vectors; i++) {
+        for (int k = 0; k < vector_size; k++) {
+            adpfloat_tmp.Reset();
+            adpfloat_tmp.set_value(input_matrix[i][k], adpbias_input);
+            act_vec[k] = adpfloat_tmp.to_rawbits();
+        }
+        rva_in_src.rw = 1;
+        rva_in_src.data = act_vec.to_rawbits();
+        rva_in_src.addr = 0x500000 + i * 16; // Each vector takes 16 bytes (kNumVectorLanes * sizeof(ScalarType))
+        rva_in.Push(rva_in_src);
+        wait();
+    }
+    wait(); 
+
+    //GBCore SmallBuffer Config
+    rva_in_src.rw = 1;
+    rva_in_src.data = set_bytes<16>("00_00_00_10_00_00_00_00_00_00_00_00_00_00_00_00"); //base_small[5] for gamma = 0, base_small[6] for beta = 16 
+    rva_in_src.addr = set_bytes<3>("40_00_20");  // GBCoreConfig, write_index=4 
+    rva_in.Push(rva_in_src);
+    wait(); 
+
+    // Load Gamma vector to SmallBuffer (manager 5, starting at offset 0)
+    for (int k = 0; k < vector_size; k++) {
+        adpfloat_tmp.Reset();
+        adpfloat_tmp.set_value(gamma_vector[k], adpbias_gamma);
+        act_vec[k] = adpfloat_tmp.to_rawbits();
+    }
+    rva_in_src.rw = 1;
+    rva_in_src.data = act_vec.to_rawbits();
+    rva_in_src.addr = 0x600000 + 0; // SmallBuffer base + base_small[5] offset (0) * 16 bytes/vector
+    rva_in.Push(rva_in_src);
+    wait();
+
+    // Load Beta vector to SmallBuffer (manager 6, starting at offset 16)
+    for (int k = 0; k < vector_size; k++) {
+        adpfloat_tmp.Reset();
+        adpfloat_tmp.set_value(beta_vector[k], adpbias_beta);
+        act_vec[k] = adpfloat_tmp.to_rawbits();
+    }
+    rva_in_src.rw = 1;
+    rva_in_src.data = act_vec.to_rawbits();
+    rva_in_src.addr = 0x600000 + 16 * 16; // SmallBuffer base + base_small[6] offset (16) * 16 bytes/vector
+    rva_in.Push(rva_in_src);
+    wait();
+
+    //send LayerNorm start signal
+    rva_in_src.rw = 1;
+    rva_in_src.data = set_bytes<16>("00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00"); 
+    rva_in_src.addr = set_bytes<3>("00_00_30");  
+    rva_in.Push(rva_in_src);
+
+    wait();
+
+    //GBControl AXI GBControlConfig 
+    rva_in_src.rw = 1;
+    rva_in_src.data = set_bytes<16>("01_00_00_02_00_00_00_10_00_01_00_00_00_00_00_01"); //is_valid=1, mode=0, sendback=0, memory_index=0, output_memory_index=0, num_vector= 16, num_output_vector=0, num_timestep=320, num_timestep_padding=0, adpbias_act=2, adpbias_atten=0, adpbias_beta = 0, adpbias_gamma=1
+    rva_in_src.addr = set_bytes<3>("70_00_10");  // last 4 bits never used 
+    rva_in.Push(rva_in_src);
+    wait(); 
+
+    //send GBControl start signal
+    rva_in_src.rw = 1;
+    rva_in_src.data = set_bytes<16>("00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00"); 
+    rva_in_src.addr = set_bytes<3>("00_00_10");  
+    rva_in.Push(rva_in_src);
+    wait(); 
+ 
+  } // layernorm_run()
+
 }; //SC MODULE Source
 
 SC_MODULE(Dest) {
@@ -91,53 +219,143 @@ SC_MODULE(Dest) {
   sc_in<bool> rst;
   Connections::In<spec::Axi::SubordinateToRVA::Read> rva_out;
   Connections::In<spec::StreamType> data_out; 
-  Connections::In<bool> done;  
-  Connections::In<bool> pe_start;
+  Connections::In<bool> done; 
+  Connections::In<bool> pe_start; 
 
-  std::vector<spec::Axi::SubordinateToRVA::Read> dest_vec;
   spec::Axi::SubordinateToRVA::Read rva_out_dest;
+  spec::StreamType data_out_dest;
+  bool done_dest;
+  bool pe_start_dest;
+
+  bool done_PopDataOut = false;
+  bool done_PopDone = false;
+  bool done_PopPEStart = false;
+
+  std::vector<double> Output;
 
   SC_CTOR(Dest) {
-    SC_THREAD(run);
+    SC_THREAD(PopDataOut);
+    sensitive << clk.pos();
+    async_reset_signal_is(rst, false);
+    SC_THREAD(PopDone);
+    sensitive << clk.pos();
+    async_reset_signal_is(rst, false);
+    SC_THREAD(PopPEStart);
+    sensitive << clk.pos();
+    async_reset_signal_is(rst, false);
+    SC_THREAD(RVA_Out);
+    sensitive << clk.pos();
+    async_reset_signal_is(rst, false);
+    SC_THREAD(SimStop);
     sensitive << clk.pos();
     async_reset_signal_is(rst, false);
 
-  }
-
-  void run(){
+  }   
+  void RVA_Out() {
     rva_out.Reset();
     data_out.Reset();
     done.Reset();
     pe_start.Reset();
-
     wait();
-    
-    unsigned i = 0;
+
+    //unsigned i = 0;
     while (1) {
       if (rva_out.PopNB(rva_out_dest)) {
-        wait(10);
-        cout << hex << sc_time_stamp() << " Dest rva data = " << rva_out_dest.data << endl;
-        //assert(rva_out_dest.data == dest_vec[i].data);
-        i++;
+        cout << sc_time_stamp() << " Dest rva_out read data" << " \t " << endl;
+        for (int i = 0; i < spec::kNumVectorLanes; i++) {
+          AdpfloatType<8,3> tmp(rva_out_dest.data[i]);
+          //cout << tmp.to_float(2) << endl; //XXX check adativefloat bias value 
+        }
       }
-      wait(1);    
+      wait(1);
+    } //while
+  } //RVA_Out
+
+  void PopDataOut() {
+   wait();
+   while (1) {
+     if (data_out.PopNB(data_out_dest)) {
+        //cout << hex << sc_time_stamp() << " data_out data = " << data_out_dest.data << endl;
+        cout << sc_time_stamp() << " Design data_out result" << " \t " << endl;
+        for (int i = 0; i < spec::kNumVectorLanes; i++) {
+          AdpfloatType<8,3> tmp(data_out_dest.data[i]);
+          //cout << tmp.to_float(2) << endl; //XXX check adativefloat bias value 
+          Output.push_back(tmp.to_float(2));
+        }
+        // Compare  Output and golden_v and max diff must be less than 0.2
+        double diff = 0.0;
+        double max_diff = 0.0;
+        for (int i = 0; i < Output.size(); i++) {
+          diff = abs(Output[i] - golden_y[i]);
+          cout << "Output = " << Output[i] << " golden_y = " << golden_y[i] << " diff = " << diff << endl;
+          if (diff > max_diff) {
+            max_diff = diff;
+          }
+        }
+        if (max_diff > 0.2) {
+          std::cout << "TESTBENCH FAIL: max diff = " << max_diff << " greater than 0.2" << std::endl;
+          sc_assert(false);
+        }
+        else {
+          std::cout << "TESTBENCH PASS: max diff = " << max_diff << " less than 0.2" << std::endl;
+        }
+        Output.clear();
+        golden_y.clear();
+
+        
+      done_PopDataOut = true;
+     }
+     wait(); 
+   } // while
+  } //PopDataOut
+
+  void PopDone() {
+   wait();
+   while (1) {
+     if (done.PopNB(done_dest)) {
+        cout << sc_time_stamp() << " Done signal issued!!!" << " \t " << done_dest << endl;
+        done_PopDone = true;
+     }
+     wait(); 
+   } // while
+  } //PopDone
+
+  void PopPEStart() {
+   wait();
+   while (1) {
+     if (pe_start.PopNB(pe_start_dest)) {
+        cout << sc_time_stamp() << " PE_start signal issued!!!" << " \t " << pe_start_dest << endl;
+        done_PopPEStart = true;
+     }
+     wait(); 
+   } // while
+  } //PopPEStart
+
+  void SimStop() {
+    wait();
+    while(1) {
+      wait();
+      if (done_PopDataOut && done_PopDone && done_PopPEStart) {
+        std::cout << "All signals received" << std::endl;
+        sc_stop();
+      }
     }
   }
 
-}; //SC MODULE Dest
 
+}; //SC MODULE Dest
 
 SC_MODULE(testbench) {
   SC_HAS_PROCESS(testbench);
   sc_clock clk;
   sc_signal<bool> rst;
 
-  Connections::Combinational<spec::StreamType> data_in;     
-  Connections::Combinational<bool> pe_start;  
-  Connections::Combinational<bool> pe_done;
   Connections::Combinational<spec::Axi::SubordinateToRVA::Write> rva_in;
   Connections::Combinational<spec::Axi::SubordinateToRVA::Read> rva_out;
-  Connections::Combinational<spec::StreamType> data_out; 
+  Connections::Combinational<spec::StreamType> data_in;     
+  Connections::Combinational<spec::StreamType> data_out;     
+  Connections::Combinational<bool> pe_start;  
+  Connections::Combinational<bool> pe_done;  
   Connections::Combinational<bool> done;  
 
   NVHLS_DESIGN(GBModule) dut;
@@ -151,167 +369,34 @@ SC_MODULE(testbench) {
     dut("dut"),
     source("source"),   
     dest("dest")
-   {
-     dut.clk(clk);
-     dut.rst(rst);
-     dut.data_in(data_in);
-     dut.pe_start(pe_start);
-     dut.pe_done(pe_done);
-     dut.rva_in(rva_in);
-     dut.rva_out(rva_out);
-     dut.data_out(data_out);
-     dut.done(done);
+  {
 
-     source.clk(clk);
-     source.rst(rst);
-     source.data_in(data_in);
-     source.pe_done(pe_done);
-     source.rva_in(rva_in);
+    dut.clk(clk);
+    dut.rst(rst);
+    dut.rva_in(rva_in);
+    dut.rva_out(rva_out);
+    dut.data_out(data_out);
+    dut.data_in(data_in);
+    dut.done(done);
+    dut.pe_done(pe_done);
+    dut.pe_start(pe_start);
 
-     dest.clk(clk);
-     dest.rst(rst);
-     dest.rva_out(rva_out);
-     dest.data_out(data_out);
-     dest.done(done);
-     dest.pe_start(pe_start);
+    source.clk(clk);
+    source.rst(rst);
+    source.data_in(data_in);
+    source.pe_done(pe_done);
+    source.rva_in(rva_in);
 
-     testset();
+    dest.clk(clk);
+    dest.rst(rst);
+    dest.rva_out(rva_out);
+    dest.data_out(data_out);
+    dest.pe_start(pe_start);
+    dest.done(done);
 
-     SC_THREAD(run);
-   }
-
-  void testset() {
-    spec::Axi::SubordinateToRVA::Write rva_write_tmp;
-    spec::Axi::SubordinateToRVA::Read  rva_read_tmp;
- 
-    // AXI write 
-    // GB SRAM config
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = nvhls::get_rand<32>();
-    rva_write_tmp.addr = set_bytes<3>("30_00_00");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);
-    dest.dest_vec.push_back(rva_read_tmp); 
-
-    // GB Core large buffer config
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = set_bytes<16>("00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_10");
-    rva_write_tmp.addr = set_bytes<3>("40_00_10");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);
-    dest.dest_vec.push_back(rva_read_tmp);   
-    
-    // Large Buffer SRAM
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = nvhls::get_rand<32>();
-    rva_write_tmp.addr = set_bytes<3>("50_00_00");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);
-    dest.dest_vec.push_back(rva_read_tmp); 
-
-    // GB Core small buffer config
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = set_bytes<16>("00_10_00_00_00_00_00_00_00_00_00_00_00_00_00_00");
-    rva_write_tmp.addr = set_bytes<3>("40_00_20");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);
-    dest.dest_vec.push_back(rva_read_tmp);  
-
-    
-    // Small Buffer SRAM
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = nvhls::get_rand<32>();
-    rva_write_tmp.addr = set_bytes<3>("60_00_00");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);    
-    dest.dest_vec.push_back(rva_read_tmp); 
-   
-    // GBControl config
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = set_bytes<16>("00_00_00_00_00_00_00_00_00_10_00_00_00_00_00_01");
-    rva_write_tmp.addr = set_bytes<3>("70_00_10");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);
-    dest.dest_vec.push_back(rva_read_tmp); 
-
-    // LayerReduce config
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = set_bytes<16>("00_00_00_02_00_00_00_B0_00_10_00_00_00_00_00_01");
-    rva_write_tmp.addr = set_bytes<3>("80_00_10");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);
-    dest.dest_vec.push_back(rva_read_tmp);  
-    
-    // LayerNorm config
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = set_bytes<16>("01_00_00_02_00_00_01_40_00_10_00_00_00_00_00_01");
-    rva_write_tmp.addr = set_bytes<3>("90_00_10");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);
-    dest.dest_vec.push_back(rva_read_tmp);  
-
-
-    // ZeroPadding config
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = set_bytes<16>("00_02_04_02_00_64_00_60_00_10_00_00_00_00_00_01");
-    rva_write_tmp.addr = set_bytes<3>("A0_00_10");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);
-    dest.dest_vec.push_back(rva_read_tmp);  
-
-    // Attention config
-    rva_write_tmp.rw = 1;
-    rva_write_tmp.data = set_bytes<16>("00_02_04_02_00_00_00_60_00_10_00_00_00_00_00_01");
-    rva_write_tmp.addr = set_bytes<3>("B0_00_10");  // last 4 bits never used 
-    rva_read_tmp.data = rva_write_tmp.data;
-    source.src_vec.push_back(rva_write_tmp);
-    dest.dest_vec.push_back(rva_read_tmp);   
-            
-    // AXI read
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("30_00_00");
-    source.src_vec.push_back(rva_write_tmp); 
-
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("40_00_10");
-    source.src_vec.push_back(rva_write_tmp); 
-
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("50_00_00");
-    source.src_vec.push_back(rva_write_tmp);   
-
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("40_00_20");
-    source.src_vec.push_back(rva_write_tmp); 
-
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("60_00_00");
-    source.src_vec.push_back(rva_write_tmp); 
-   
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("70_00_10");
-    source.src_vec.push_back(rva_write_tmp);
-               
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("80_00_10");
-    source.src_vec.push_back(rva_write_tmp);    
-
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("90_00_10");
-    source.src_vec.push_back(rva_write_tmp); 
-
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("A0_00_10");
-    source.src_vec.push_back(rva_write_tmp);
-
-    rva_write_tmp.rw = 0;
-    rva_write_tmp.addr = set_bytes<3>("B0_00_10");
-    source.src_vec.push_back(rva_write_tmp); 
-
+    SC_THREAD(run); 
   }
   
-
-
   void run(){
     wait(2, SC_NS );
     std::cout << "@" << sc_time_stamp() <<" Asserting reset" << std::endl;
@@ -319,25 +404,18 @@ SC_MODULE(testbench) {
     wait(2, SC_NS );
     rst.write(true);
     std::cout << "@" << sc_time_stamp() <<" De-Asserting reset" << std::endl;
-    wait(10000, SC_NS );
-    std::cout << "@" << sc_time_stamp() <<" sc_stop" << std::endl;
+    wait(100000, SC_NS );
+    std::cout << "@" << sc_time_stamp() <<" Timeout Error!" << std::endl;
+    sc_assert(false);
     sc_stop();
-  }  
-
-}; //SC MODULE testbench
-
+  }
+}; //SC Module testbench
 
 int sc_main(int argc, char *argv[]) {
-  nvhls::set_random_seed();
-  NVINT8 test = 14;
-  cout << fixed2float<8, 3>(test) << endl;
-
   
   testbench tb("tb");
-  
   sc_report_handler::set_actions(SC_ERROR, SC_DISPLAY);
   sc_start();
-
 
   bool rc = (sc_report_handler::get_count(SC_ERROR) > 0);
   if (rc)
@@ -346,3 +424,5 @@ int sc_main(int argc, char *argv[]) {
     DCOUT("TESTBENCH PASS" << endl);
   return rc;
 }
+
+
