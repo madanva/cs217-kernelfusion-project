ca9d41fd69b4843dd5507a8851822c97bbdcc539
Ubuntu <ubuntu@ip-172-31-93-178.ec2.internal>
2026-02-08 19:50:27 +0000

    intwermediate foga test

--- design_top/software/src/design_top.h
+++ design_top/software/src/design_top.h
@@ -15,61 +15,60 @@
 #ifndef DESIGN_TOP_H
 #define DESIGN_TOP_H
 
-#include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
 
 // ============================================================================
-// GBModule Interface Constants
+// Top Module Interface Parameters from design_top_defines.vh
 // ============================================================================
 
-// Vector and data widths
-#define kVectorSize 16
-#define kAdpfloatWordWidth 8
-#define kAdpfloatExpWidth 3
-#define kAdpfloatBiasWidth 3
+// AXI Write Address Channel (Host to Top)
+#define WIDTH_TOP_AXI_AW 50
+#define LOOP_TOP_AXI_AW ((WIDTH_TOP_AXI_AW + 31) / 32) // 2 words
+#define ADDR_TOP_AXI_AW_START 0x400
 
-// AXI interface parameters
+// AXI Write Data Channel (Host to Top)
+#define WIDTH_TOP_AXI_W 145
+#define LOOP_TOP_AXI_W ((WIDTH_TOP_AXI_W + 31) / 32) // 5 words
+#define ADDR_TOP_AXI_W_START 0x410
+
+// AXI Write Response Channel (Top to Host)
+#define WIDTH_TOP_AXI_B 12
+#define LOOP_TOP_AXI_B ((WIDTH_TOP_AXI_B + 31) / 32) // 1 word
+#define ADDR_TOP_AXI_B_START 0x430
+
+// AXI Read Address Channel (Host to Top)
+#define WIDTH_TOP_AXI_AR 50
+#define LOOP_TOP_AXI_AR ((WIDTH_TOP_AXI_AR + 31) / 32) // 2 words
+#define ADDR_TOP_AXI_AR_START 0x440
+
+// AXI Read Data Channel (Top to Host)
+#define WIDTH_TOP_AXI_R 141
+#define LOOP_TOP_AXI_R ((WIDTH_TOP_AXI_R + 31) / 32) // 5 words
+#define ADDR_TOP_AXI_R_START 0x450
+
+// Interrupt (Top to Host)
+#define ADDR_TOP_INTERRUPT 0x570 // Read
+
+// FPGA OCL
 #define WIDTH_AXI 32
 #define ADDR_WIDTH_OCL 16
 
-// RVA interface widths
-// rva_in: data(128) + addr(24) + wstrb(16) + rw(1) = 169 bits
-#define kRVADataWidth 128
-#define kRVAAddrWidth 24
-#define kRVAWstrbWidth 16
-#define kRVAInWidth 169
+// ============================================================================
+// Data Structures
+// ============================================================================
 
-// rva_out: 128 bits
-#define kRVAOutWidth 128
+typedef struct {
+    uint32_t addr;
+    uint32_t data[4]; // 128 bits
+} AxiWriteCommand;
 
-// Number of 32-bit words for RVA interface
-#define LOOP_RVA_IN ((kRVAInWidth + 31) / 32)   // 6 words
-#define LOOP_RVA_OUT ((kRVAOutWidth + 31) / 32) // 4 words
+typedef struct {
+    uint32_t addr;
+    uint32_t data[4]; // 128 bits
+    uint32_t expected_read_data[4]; // 128 bits
+} AxiReadCommand;
 
-// Transfer Counter addresses
-#define ADDR_TX_COUNTER_EN 0x0400        // Write
-#define ADDR_TX_COUNTER_READ 0x0400      // Read
-#define ADDR_COMPUTE_COUNTER_READ 0x0404 // Read
-
-// Start enable address
-#define ADDR_START_CFG 0x0404 // Write
-
-// RVA IN Port (Input) - base address for writing RVA messages
-#define ADDR_RVA_IN_START 0x0408
-
-// RVA Out Port (Output) - base address for reading RVA responses
-// Calculated as: ADDR_RVA_IN_START + LOOP_RVA_IN * 4 + output_port offset
-#define ADDR_RVA_OUT_START 0x044C
-
-// Address prefixes for RVA routing (bits [23:20] of address)
-#define ADDR_PREFIX_SRAM_CFG 0x3
-#define ADDR_PREFIX_GBCORE_CFG 0x4
-#define ADDR_PREFIX_GBCORE_DATA 0x5
-#define ADDR_PREFIX_NMP_CFG 0xC
-
-// Number of banks in GBCore large buffer
-#define kNumBanks 8
 
 // ============================================================================
 // Function Prototypes
@@ -79,29 +78,8 @@
 int ocl_wr32(int bar_handle, uint16_t addr, uint32_t data);
 int ocl_rd32(int bar_handle, uint16_t addr, uint32_t* data);
 
-// RVA interface functions
-void pack_rva_in(
-    bool rw,
-    uint32_t addr,
-    const uint32_t data[LOOP_RVA_OUT],
-    uint32_t rva_msg[LOOP_RVA_IN]);
-int ocl_rva_wr(int bar_handle, const uint32_t rva_msg[LOOP_RVA_IN]);
-int ocl_rva_rd(int bar_handle, uint32_t rva_data[LOOP_RVA_OUT]);
-
-// Configuration helper functions
-void make_gbcore_cfg_data(
-    uint8_t num_vec, uint16_t base, uint32_t data[LOOP_RVA_OUT]);
-void make_nmp_cfg_data(
-    uint8_t mode,
-    uint8_t mem,
-    uint8_t nvec,
-    uint16_t ntimestep,
-    uint8_t adpbias,
-    uint32_t data[LOOP_RVA_OUT]);
-uint32_t make_gbcore_data_addr(uint16_t local_index);
-
-// Control functions
-int send_start(int bar_handle);
-int wait_for_done(int bar_handle);
+// Top-level AXI interface functions
+int top_write(int bar_handle, const AxiWriteCommand* write_command);
+int top_read(int bar_handle, AxiReadCommand* read_command);
 
 #endif // DESIGN_TOP_H
